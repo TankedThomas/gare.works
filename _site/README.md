0275b8e3a157a2e7ff863aaa65abccca29ac0a94
TankedThomas <TankedThomas@users.noreply.github.com>
2022-03-02 19:11:54 +1300

    First draft site

---
+++ _site/README.md
@@ -0,0 +1,5 @@
+# www.gare.works
+
+This is the repository for www.gare.works
+
+No unauthorised reproduction is allowed.
---
+++ _site/js/main.js
@@ -0,0 +1,2 @@
+// Set Copyright Date in Footer
+document.getElementById("copydate").innerHTML = newDate().getFullYear();
