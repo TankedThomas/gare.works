d5906961960e59d4df5f67b8315148819db71bb3
TankedThomas <TankedThomas@users.noreply.github.com>
2022-09-14 23:03:51 +1200

    Update admin config and reformat posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Contact Form Currently Broken",
-        "excerpt":"Please note that our contact form is currently broken. It will be fixed soon and this message will be removed once fixed.   In the interim, please see the attached poster for contact details.  ","categories": ["news"],
+        "excerpt":"Please note that our contact form is currently broken. It will be fixed soon and this message will be removed once fixed.   In the interim, please see the attached poster for contact details.     ","categories": ["news"],
         "tags": [],
         "url": "/news/2022/09/14/contact_form_currently_broken.html",
         "teaser": null
